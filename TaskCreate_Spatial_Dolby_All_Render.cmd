696c16eab2071f549a0b53728ce618728a6d3f72
bandaid <clarkb@students.westerntc.edu>
2026-02-19 05:00:36 -0600

    Enhance TaskCreate_Spatial_Dolby_All_Render.cmd
    Added echo and pause commands for better user feedback.

--- TaskCreate_Spatial_Dolby_All_Render.cmd
+++ TaskCreate_Spatial_Dolby_All_Render.cmd
@@ -1,3 +1,7 @@
+echo Run as Administrator
+
 schtasks /Create /TN "Force_Spatial_Dolby_All_Render" ^
   /TR "\"%USERPROFILE%\NirTools\Force_Spatial_Dolby_All_Render.cmd\"" ^
   /SC ONLOGON /RL HIGHEST /F
+
+pause
